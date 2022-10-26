47b09f5cf8d724cbdfa6563447a68ea195fce331
Tikkimik <googlbubu@gmail.com>
2022-10-27 00:12:57 +0300

    error hendler test

---
+++ src/test/java/ru/practicum/shareit/exceptions/ErrorHandlerTest.java
@@ -0,0 +1,35 @@
+package ru.practicum.shareit.exceptions;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+class ErrorHandlerTest {
+
+    ErrorHandler errorHandler;
+
+    @BeforeEach
+    void beforeEach() {
+        errorHandler = new ErrorHandler();
+    }
+
+    @Test
+    void handleNotFoundParameterException() {
+        ErrorHandler errorHandler = new ErrorHandler();
+        NotFoundParameterException exception = new NotFoundParameterException("bad");
+        errorHandler.handleNotFoundParameterException(exception);
+        MatcherAssert.assertThat("NotFoundParameterException \"" + exception.getMessage() +"\".",
+                equalTo(errorHandler.handleNotFoundParameterException(exception).getError()));
+    }
+
+    @Test
+    void handleThrowableException() {
+        ErrorHandler errorHandler = new ErrorHandler();
+        Throwable exception = new Throwable("bad");
+        errorHandler.handleThrowableException(exception);
+        MatcherAssert.assertThat(exception.getMessage(),
+                equalTo(errorHandler.handleThrowableException(exception).getError()));
+    }
+}
